2ee2f569ae27c97263e42526bc7e099c7c7d6713
Герасимчик Маргарита <78028468+MagaClown@users.noreply.github.com>
2021-02-02 12:11:05 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int n;
+	n = 5;
+	
+    printf("Hello world");
+    printf("This is my first program.");
+    printf("I'm learning C programming.");
+    
+  
+  system("PAUSE");	
+  return 0;
+}
+
+
+
